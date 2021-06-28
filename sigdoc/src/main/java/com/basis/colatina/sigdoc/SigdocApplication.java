8441942ff1b8a2fac10b3750aaa3ea6973ef9c06
Maxson Zucolotto <maxson.zucolotto@basis.com.br>
2021-06-28 19:12:23 -0300

    Commit inicial

---
+++ sigdoc/src/main/java/com/basis/colatina/sigdoc/SigdocApplication.java
@@ -0,0 +1,13 @@
+package com.basis.colatina.sigdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigdocApplication.class, args);
+	}
+
+}
---
+++ sigdoc/src/test/java/com/basis/colatina/sigdoc/SigdocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basis.colatina.sigdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigdocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
