06f9377a837084ba837810bc688b7c40c5dc66c9
Alisson Vighini <alisson.vighini@basis.com.br>
2021-06-29 19:53:50 -0300

    atualizando o pom

--- sigdoc/src/main/java/com/basis/colatina/sigdoc/service/TaskService.java
+++ sigdoc/src/main/java/com/basis/colatina/sigdoc/service/TaskService.java
@@ -1,9 +1,13 @@
 package com.basis.colatina.sigdoc.service;
 
 import com.basis.colatina.sigdoc.repository.TaskRepository;
+import com.basis.colatina.sigdoc.service.dto.TaskListDTO;
+import com.basis.colatina.sigdoc.service.mapper.TaskListMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -11,5 +15,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class TaskService {
 
     private final TaskRepository taskRepository;
+
+    private final TaskListMapper taskListMapper;
+
+    public List<TaskListDTO> findAllTasks(){
+        return taskListMapper.toDto(taskRepository.findAll());
+    }
 
 }
